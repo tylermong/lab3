1efe818f2a136496db8ff9ee01517c068e63049c
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2024-10-02 10:25:42 -0400

    Added another print statement

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello
     public static void main(String argv[])
     {
         System.out.println("Hello CSCI4485 " + argv[0] + "!");
+        System.out.println("Hello Csci4485 " + argv[0] + '!');
     }
 }
